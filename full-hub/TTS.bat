73679c146b1e7de8619c810081c0ed52488c176a
xxxiu <shuaigew94@gmail.com>
2025-12-23 19:48:55 +0800

    修复full-hub文件夹下面代码出现重复叠加的bug

--- full-hub/TTS.bat
+++ full-hub/TTS.bat
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 @echo off
 :: 使用脚本所在目录作为工作目录
 cd %~dp0tts-hub\GPT-SoVITS-Bundle
 set "PATH=%~dp0tts-hub\GPT-SoVITS-Bundle\runtime;%PATH%"
 runtime\python.exe api.py -p 5000 -d cuda -s role_voice_api/neuro/merge.pth -dr role_voice_api/neuro/01.wav -dt "Hold on please, I'm busy. Okay, I think I heard him say he wants me to stream Hollow Knight on Tuesday and Thursday." -dl "en"
 pause
-=======
-@echo off
-:: 使用脚本所在目录作为工作目录
-cd %~dp0tts-hub\GPT-SoVITS-Bundle
-set "PATH=%~dp0tts-hub\GPT-SoVITS-Bundle\runtime;%PATH%"
-runtime\python.exe api.py -p 5000 -d cuda -s role_voice_api/neuro/merge.pth -dr role_voice_api/neuro/01.wav -dt "Hold on please, I'm busy. Okay, I think I heard him say he wants me to stream Hollow Knight on Tuesday and Thursday." -dl "en"
-pause
->>>>>>> 5c9f8e0 (更新)

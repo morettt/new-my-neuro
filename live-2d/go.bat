ae9fad31d0b5fda9d1940091d344652b807f8f8c
xxxiu <shuaigew94@gmail.com>
2025-12-24 23:16:51 +0800

    修改布局，修改代码

--- live-2d/go.bat
+++ live-2d/go.bat
@@ -2,4 +2,11 @@
 chcp 65001
 cd /d %~dp0
 echo 正在启动桌宠应用...
-node .\node_modules\electron\cli.js .
+
+if exist ".\node\node.exe" (
+    echo 使用项目自带的 Node.js 环境
+    .\node\node.exe .\node_modules\electron\cli.js .
+) else (
+    echo 未找到项目自带的 Node.js，使用系统环境的 Node.js
+    node .\node_modules\electron\cli.js .
+)
